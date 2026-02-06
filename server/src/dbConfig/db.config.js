9ee5fa59eb3bf43e4534a79c6c5e3f2cdc6e9cb0
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-06 14:16:38 +0530

    created database connection

---
+++ server/src/dbConfig/db.config.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+const DbName = process.env.DB_NAME
+
+export const  Db_Connection = async ()=> {
+    try {
+        const connection = await mongoose.connect(`${MONGODB_URI}${DbName}`)
+        console.log('Mongo Db connected to '+DbName);
+        
+    } catch (error) {
+        console.log('Error connecting to db');
+        process.exit(1)
+    }
+}
+
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import {Db_Connection} from './dbConfig/db.config.js'
 dotenv.config()
 
 const PORT = process.env.PORT;
 
 const app = express()
 
-app.listen(()=> {
+app.listen(PORT,()=> {
     console.log('app running on port '+ PORT);
     
 })
 
+Db_Connection()
