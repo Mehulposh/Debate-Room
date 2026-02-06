63c7e2797c421975d463445173b74d42a4764dc2
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-06 13:59:23 +0530

    created a server using node and express

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT;
+
+const app = express()
+
+app.listen(()=> {
+    console.log('app running on port '+ PORT);
+    
+})
+
