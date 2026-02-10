52ff27ca61f1565ae8297b7c55cfa21f43bfd24a
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-10 12:41:57 +0530

    refactor server setup to use HTTP server with Socket.IO integration

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import {Db_Connection} from './dbConfig/db.config.js'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = process.env.PORT;
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
+import { setupSocket } from "./socket.js";
+import { Db_Connection } from "./dbConfig/db.config.js";
 
-const app = express()
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> {
-    console.log('app running on port '+ PORT);
-    
-})
+// DB connection
+Db_Connection();
 
-Db_Connection()
+// Create HTTP server
+const server = http.createServer(app);
+
+// Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT" , "DELETE" ]
+  }
+});
+
+// Setup socket logic
+setupSocket(io);
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
