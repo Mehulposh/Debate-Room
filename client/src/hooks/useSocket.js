2953a2906e6569a00aabfdd7a144248c56c1dec8
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-11 10:42:34 +0530

    fix: correct import statements in useAuth and useSocket hooks

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import {SocketContext} from '../context/socketContext.jsx'
+import SocketContext from '../context/socketContext.jsx'
 
 
 export const useSocket = () => {
