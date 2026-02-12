6463d6e553d35ed6d8ae5bad5ee56aed73cd6c5f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-12 21:38:04 +0530

    feat: added debate card component

--- client/src/components/DebateCard.jsx
+++ client/src/components/DebateCard.jsx
@@ -3,6 +3,9 @@ import { formatDistanceToNow } from 'date-fns';
 
 
 const DebateCard = ({debate}) => {
+  console.log('Debate id', debate._id);
+  console.log("creator:", debate.creator);
+  if(!debate) return null
   return (
     <div className="card-body w-full ">
         <div className="flex flex-col items-center gap-3">
@@ -19,21 +22,26 @@ const DebateCard = ({debate}) => {
         <div className="flex justify-around items-center mt-3" >
               <span className="flex flex-col gap-2 text-success font-semibold" >
                 <Users size={16} />
-                {debate.participants.length}
+                {debate.participants?.length || 0}
               </span>
               <span  className="flex flex-col gap-2 text-secondary font-semibold" >
                 <MessageSquare size={16} />
-                {debate.arguments.length}
+                {debate.arguments?.length || 0}
               </span>
               <span  className="flex flex-col gap-2 text-accent font-semibold" >
                 <Clock size={16} />
-                {formatDistanceToNow(new Date(debate.createdAt.$date), { addSuffix: true })}
+                {debate.createdAt
+                    ? formatDistanceToNow(new Date(debate.createdAt), {
+                        addSuffix: true,
+                    })
+                    : "Unknown time"
+                }
               </span>
         </div>
 
         <div >
             <small className=" text-info">
-                Created by : <strong className="text-warning">{debate.creator.username || 'ddgwgwgrwrg'}</strong>
+                Created by : <strong className="text-warning">{debate.creator?.username || 'ddgwgwgrwrg'}</strong>
             </small>
         </div>
     </div>
