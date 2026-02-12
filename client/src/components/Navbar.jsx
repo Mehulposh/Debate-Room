ebea404db302ba98aa62ce9fb9e031eec18a0d70
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-12 21:33:39 +0530

    refactor: refactored navbar to use auth hook and improve styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { LogOut, User, MessageSquare } from 'lucide-react';
 
 const Navbar = () => {
     //extract user and logout function from auth context
-    // const {user, logout}= useAuth()
-    const user = true
+    const {user, logout}= useAuth()
+    // const user = true
     const navigate = useNavigate
     //logout handler
     const handleLogout = () => {
@@ -16,7 +16,7 @@ const Navbar = () => {
   };
 
   return(
-    <nav className='p-5  bg-neutral '>
+    <nav className='p-5 w-screen  bg-neutral sticky top-0 z-50'>
         <div className='flex items-center justify-between '>
             <Link to='/' className='flex items-center gap-2 btn btn-primary'>
                 <MessageSquare size={28}/>
@@ -24,13 +24,13 @@ const Navbar = () => {
             </Link>
 
             {user && (
-                <div className='flex items-center justify-between gap-5'>
-                    <Link to='/debates' className='text-lg font-semibold' >
+                <div className='flex items-center justify-between  w-1/3  gap-5'>
+                    <Link to='/debates' className='text-lg font-semibold btn btn-primary' >
                         Debates
                     </Link>
-                    <Link to='/profile' className='text-lg font-semibold'>
+                    <Link to='/profile' className='text-sm font-semibold flex items-center gap-2 pb-1 border-b border-primary'>
                         <User size={18}/>
-                        {user.username}
+                        <span  className="flex flex-wrap max-w-[100px] leading-tight">{user.username}</span>
                     </Link>
                     <button className='btn btn-primary flex items-center' onClick={handleLogout}>
                         <LogOut size={18} />
