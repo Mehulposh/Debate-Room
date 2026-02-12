0841bdd40163311cbc358259ed028c2419c7d34e
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-12 12:07:31 +0530

    feat: added a warning component for speaker time limit

---
+++ client/src/components/TimeLimitWarning.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimeLimitWarning = () => {
+  return (
+    <div className='border bg-warning text-lg'>
+        ⚠️ Time limit exceeded!
+    </div>
+  )
+}
+
+export default TimeLimitWarning
