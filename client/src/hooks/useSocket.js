ddfdd3eddd026ec1f763ecef05432bc31bebf545
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-12 21:43:52 +0530

    refactor: debugged the import for authContext and seperated the socketprovider function

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,16 +1,8 @@
 import { useContext } from "react";
-import SocketContext from '../context/socketContext.jsx'
+import {SocketContext} from '../context/socketContext.jsx'
 
 
 export const useSocket = () => {
     //using useContext to use SocketContext 
-    const context = useContext(SocketContext)
-
-    //error handling in case the context is not created
-    if (!context) {
-        throw new Error('useSocket must be used within a SocketProvider');
-    }
-
-    //return the context
-    return context;
+     return useContext(SocketContext);
 }
