6047b171d972ecf1c7aab358e3caafa2acbf86b5
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-02-12 21:42:28 +0530

    refactor: improved cors access

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,15 @@ import debateRoutes from "./routes/debate.routes.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
